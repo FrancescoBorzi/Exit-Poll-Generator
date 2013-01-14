91f2b02f71b7680fb781ef42597916135f0dd7e8
GigaDev90 <gigante.gabriele@hotmail.it>
2013-01-14 11:23:45 +0100

    first commit

---
+++ src/exit/poll/generator/ExitPollGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exit.poll.generator;
+
+/**
+ *
+ * @author Gabriele
+ */
+public class ExitPollGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
