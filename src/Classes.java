ebcf3bd3db5e475a3820eb09bc1a8e084205bca7
Shin Darth <shin.darth@hotmail.it>
2013-01-16 14:46:14 +0100

    More classes

--- src/Classes.java
+++ src/Classes.java
@@ -1,52 +1,108 @@
 import java.util.Random;
 
+class DataBase
+{
+    private Coalition[] coalitions_db;
+    private Party[] parties_db;
+    private int pIndex, cIndex;
+    private int cAmount;
+    private int pAmount;
+    
+    public DataBase(int cAmount, int pAmount)
+    {
+        this.cAmount = cAmount;
+        this.pAmount = pAmount;
+        
+        coalitions_db = new Coalition[cAmount];
+        parties_db = new Party[pAmount];
+        
+        pIndex = cIndex = 0; 
+    }
+    
+    public int regCoalition(Coalition c)
+    {
+        if (cIndex >= coalitions_db.length)
+            return -1;
+        
+        coalitions_db[cIndex] = c;
+        cIndex++;
+        
+        return cIndex;
+    }
+    
+    public int regParty(Party p)
+    {
+        if (pIndex >= parties_db.length)
+            return -1;
+        
+        parties_db[pIndex] = p;
+        pIndex++;
+        
+        return pIndex;
+    }
+    
+    public float getPercentage(Coalition c)
+    {
+        // TODO
+        return 0;
+    }
+    
+    public float getPercentage(Party p)
+    {
+        // TODO
+        return 0;
+    }
+    
+    public void reset()
+    {
+        coalitions_db = new Coalition[cAmount];
+        parties_db = new Party[pAmount];
+        pIndex = cIndex = 0;
+    }
+}
+
 class Party
 {
-    private int id, coalition, votes, percentage;
+    private int id, coalition, votes;
     private String name;
     private String logo;
+    private DataBase db;
     
-    public Party(int id, int coalition, String name, String logo)
+    public Party(DataBase db, Coalition coalition, String name, String logo, int votes)
     {
-        this.id = id; // must be > 0
-        this.coalition = coalition;
+        this.db = db;
+        this.id = db.regParty(this);
+        this.coalition = coalition.getId();
         this.name = name;
         this.logo = logo;
-        
-        votes = 0;
-        percentage = 0;
+        this.votes = votes;
     }
     
     public int getId()          { return id; }
     public int getCoalition()   { return coalition; }
     public int getVotes()       { return votes; }
-    public int getPercentage()  { return percentage; }
     public String getName()     { return name; }
     public String getLogo()     { return logo; }
     
-    public void setId(int i)          { id = i; }
-    public void setCoalition(int i)   { coalition = i; }
-    public void setVotes(int i)       { votes = i; }
-    public void setPercentage(int i)  { percentage = i; }
-    public void setName(String i)     { name = i; }
-    public void setLogo(String i)     { logo = i; }
+    public float getPercentage()  { return db.getPercentage(this); }
 }
 
 class Coalition
 {
-    private int id, votes, percentage, partiesAddedCount;
+    private int id, votes, partiesAddedCount;
     private Party[] parties;
     private String name;
     private String logo;
+    private DataBase db;
     
-    public Coalition(int id, String name, String logo, int parties)
+    public Coalition(DataBase db, String name, String logo, int parties)
     {
-        this.id = id; // must be > 0
+        this.db = db;
+        this.id = db.regCoalition(this);
         this.name = name; // President's name
         this.logo = logo;
         
         votes = 0;
-        percentage = 0;
         
         this.parties = new Party[parties]; // parties number must be <= 4
         partiesAddedCount = 0; // must be <= 4
@@ -54,15 +110,10 @@ class Coalition
     
     public int getId()          { return id; }
     public int getVotes()       { return votes; }
-    public int getPercentage()  { return percentage; }
     public String getName()     { return name; }
     public String getLogo()     { return logo; }
     
-    public void setId(int i)          { id = i; }
-    public void setVotes(int i)       { votes = i; }
-    public void setPercentage(int i)  { percentage = i; }
-    public void setName(String i)     { name = i; }
-    public void setLogo(String i)     { logo = i; }
+    public float getPercentage()  { return db.getPercentage(this); }
     
     public void addParty(Party p)
     {
@@ -71,6 +122,16 @@ class Coalition
         
         parties[partiesAddedCount] = p;
         partiesAddedCount++;
+        votes += p.getVotes();
+    }
+    
+    public void updateVotes()
+    {
+        votes = 0;
+        for (int i = 0; i < partiesAddedCount; i++)
+        {
+            votes += parties[i].getVotes();
+        }
     }
     
     public void resetParties()
@@ -81,7 +142,6 @@ class Coalition
         partiesAddedCount = 0;
     }
 }
-
 
 class Sample
 {
